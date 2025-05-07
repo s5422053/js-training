a13cfa85b488c8ca4a1af7f88d2ea59a5493795b
s5422053 <chda22053@g.nihon-u.ac.jp>
2025-05-07 14:40:54 +0900

    lesson4 done

--- src/pages/Lesson04/index.jsx
+++ src/pages/Lesson04/index.jsx
@@ -4,7 +4,23 @@ import Chart from "../../components/Chart04";
 import instruction from "./instruction.md?raw";
 
 const convertData = (input) => {
-  return []; // ここを作りましょう！
+
+  let data = [];
+  for(let i=0;i<input.length;i++){
+    let speciesExist = -1
+    for(let j=0;j<data.length;j++){
+      if(input[i].species === data[j].id){
+        speciesExist = j;
+      }
+    }
+    if(speciesExist === -1){
+      data.push({id:input[i].species,data:[]});
+    }else{
+      data[speciesExist].data.push({x:input[i].sepalLength,y:input[i].petalWidth});
+    }
+  }
+
+  return data; // ここを作りましょう！
 };
 
 const Lesson = () => {
