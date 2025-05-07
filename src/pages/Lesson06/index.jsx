2e3f8697731250cd6ec9995ca3beddbb560fd86e
s5422053 <chda22053@g.nihon-u.ac.jp>
2025-05-07 15:21:49 +0900

    lesson6 done

--- src/pages/Lesson06/index.jsx
+++ src/pages/Lesson06/index.jsx
@@ -4,7 +4,25 @@ import Chart from "../../components/Chart06";
 import instruction from "./instruction.md?raw";
 
 const convertData = (input) => {
-  return []; // ここを作りましょう！
+  let data = [];
+  for(let i=0;i<input.length;i++){
+    let color;
+    if(input[i].gender === "男性"){
+      color = "blue";
+    }else{
+      color = "red";
+    }
+    data.push(
+      {
+        color:color,
+        gender:input[i].gender,
+        bmi:input[i].y/(input[i].x ** 2),
+        weight:input[i].x,
+        height:input[i].y
+      }
+    );
+  }
+  return data; // ここを作りましょう！
 };
 
 const Lesson = () => {
